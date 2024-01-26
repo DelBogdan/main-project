9d92b67fc45f708b4cd82de67c6df9a897836b94
Del Bogdan <delbogdan@yandex.ru>
2024-01-26 20:28:47 +0300

    some changes

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,6 @@ public class Main {
 
         System.out.println("Для наполнения бассейна потребовалось " + minuteCounter + " минут");
         System.out.println("Для проверки Git");
+        System.out.println("Для проверки Git");
     }
 }
