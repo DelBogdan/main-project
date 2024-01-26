852de0bf7606cf819fa86afbfb2da128c7f9098d
Del Bogdan <delbogdan@yandex.ru>
2024-01-26 19:25:28 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int volume = 1200; // объем бассейна
+        int fillingSpeed = 30; //30 litres per minute
+        int devastationSpeed = 10; //10 litres per minute
+        int sumLitres = 0; // текущий объем
+        int minuteCounter = 0; // счетчик минут
+
+        while (true) {
+            if (sumLitres < volume) {
+                sumLitres = sumLitres + fillingSpeed - devastationSpeed;
+                minuteCounter++;
+            } else
+                break;
+        }
+
+        System.out.println("Для наполнения бассейна потребовалось " + minuteCounter + " минут");
+
+    }
+}
